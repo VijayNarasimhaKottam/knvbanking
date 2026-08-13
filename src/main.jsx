b9c2b76a6ecaf35c4ec19b090dfb40cbe1cc334b
VijayNarasimhaKottam <kottamvijaynarasimha@gmail.com>
2026-08-13 10:34:57 +0530

    Fix missing CSS variables

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import './index.css';
 import './styles/base.css';
 import './styles/layout.css';
 import './styles/components.css';
