c1f97eacf95caf4b6f986cf46d49f44941d294f7
VijayNarasimhaKottam <kottamvijaynarasimha@gmail.com>
2026-08-13 11:50:36 +0530

    Remove Vite boilerplate CSS restricting width

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
 import './styles/base.css';
 import './styles/layout.css';
 import './styles/components.css';
